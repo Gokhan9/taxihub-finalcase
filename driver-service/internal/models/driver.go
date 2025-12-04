1371e54a83664c6f4f063298b770a5916c2bdb72
Gokhan9 <gcancomert@hotmail.com>
2025-12-04 13:03:14 +0300

    Puanlama sistemi eklemek için driver modeline rating(type:float64) ve ratingCount(type:int) alanlarını ekledim.

--- driver-service/internal/models/driver.go
+++ driver-service/internal/models/driver.go
@@ -19,17 +19,18 @@ const (
 )
 
 type Driver struct {
-	ID        primitive.ObjectID `bson:"_id,omitempty" json:"id"` //Belgeyi mongo'ya "primitive _id" olarak kaydeder. - id(string) - objectID tipine çevirir.
-	FirstName string             `bson:"firstName" json:"firstName"`
-	LastName  string             `bson:"lastName" json:"lastName"`
-	Plate     string             `bson:"plate" json:"plate"`
-	TaxiType  string             `bson:"taxiType" json:"taxiType"`
-	CarBrand  string             `bson:"carBrand" json:"carBrand"`
-	CarModel  string             `bson:"carModel" json:"carModel"`
-	Location  Location           `bson:"location" json:"location"`
-	CreatedAt time.Time          `bson:"createdAt" json:"createdAt"`
-	UpdatedAt time.Time          `bson:"updatedAt" json:"updatedAt"`
-	Distance  float64            `json:"distance,omitempty"`
-	Status    string             `bson:"status" json:"status"` // YENİ ALAN: Sürücünün o anki durumu. json:"status" -> API'nin yanıtını döner, bson:"status" -> veritabanında bu ad ile saklanır.
-
+	ID          primitive.ObjectID `bson:"_id,omitempty" json:"id"` //Belgeyi mongo'ya "primitive _id" olarak kaydeder. - id(string) - objectID tipine çevirir.
+	FirstName   string             `bson:"firstName" json:"firstName"`
+	LastName    string             `bson:"lastName" json:"lastName"`
+	Plate       string             `bson:"plate" json:"plate"`
+	TaxiType    string             `bson:"taxiType" json:"taxiType"`
+	CarBrand    string             `bson:"carBrand" json:"carBrand"`
+	CarModel    string             `bson:"carModel" json:"carModel"`
+	Location    Location           `bson:"location" json:"location"`
+	CreatedAt   time.Time          `bson:"createdAt" json:"createdAt"`
+	UpdatedAt   time.Time          `bson:"updatedAt" json:"updatedAt"`
+	Distance    float64            `json:"distance,omitempty"`
+	Status      string             `bson:"status" json:"status"` // YENİ ALAN: Sürücünün o anki durumu. json:"status" -> API'nin yanıtını döner, bson:"status" -> veritabanında bu ad ile saklanır.
+	Rating      float64            `bson:"rating" json:"rating"`
+	RatingCount int                `bson:"ratingCount" json:"ratingCount"`
 }
